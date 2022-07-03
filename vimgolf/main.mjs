c6621bcf2420ceaca28b8f9a6d88d79945f384b5
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-07-03 15:36:23 +0300

    Update VimGolf helper script

--- vimgolf/main.mjs
+++ vimgolf/main.mjs
@@ -1,5 +1,6 @@
 import challenges from "./challenges.json" assert { type: "json" };
 import { execSync } from "child_process";
+import { readFileSync } from "fs";
 
 const args = process.argv.slice(2);
 
@@ -13,16 +14,45 @@ if (args.includes("--all")) {
 
 /* FUNCTIONS */
 
-async function loadChallenge(challenge) {
-  const { id, score, name } = challenge;
-  console.log(`🥷  Next challenge: ${name}`);
-  console.log(`🧩 Lowest score: ${score}`);
+async function loadChallenge(challenge, repeat = false) {
+  let { id, score, name } = challenge;
+  console.log(`\n🥷  ${repeat ? "Repeating" : "Next"} challenge: ${name}`);
+  console.log(`🎲 Lowest score: ${score}`);
+  await new Promise((r) => setTimeout(r, 2000));
 
-  await new Promise((r) => setTimeout(r, 1000));
-  const output = execSync(`vimgolf put ${id}`, { stdio: "inherit" });
+  execSync(`vimgolf put ${id}`, { stdio: "inherit" });
+
+  const logFile = readFileSync("./console.log", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  const scoreLine = logFile
+    .split("\r\n")
+    .reverse()
+    .find((line) => line.includes("Your score"));
+
+  if (scoreLine.toLowerCase().includes("fail")) return;
+  else if (scoreLine.toLowerCase().includes("success")) {
+    const [currentScore] = scoreLine.match(/(?<=: )\d+/);
+    if (currentScore == score) {
+      console.log("💪 Good job! On to the next challenge! 🧑‍💻");
+    } else if (currentScore > score) {
+      console.log(
+        `🧩 The challenge can still be optimized to a score of ${score}, try again! 🤔`
+      );
+      return loadChallenge(challenge, true);
+    } else if (currentScore < score) {
+      for (let i = 1; i <= 3; i += 1)
+        console.log(
+          "🤯 Wow! A new solution found! Update the challenges file! 🤩"
+        );
+      process.exit();
+    } else throw new Error("Unknown scoring.");
+  } else throw new Error("Unhandled score line format.");
 }
 
 async function playAllChallenges() {
+  if (args.includes("--random")) challenges.sort(() => 0.5 - Math.random());
   for (const challenge of challenges) {
     await loadChallenge(challenge);
   }
