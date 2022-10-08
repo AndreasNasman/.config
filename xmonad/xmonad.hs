9693c98bdd96c3e821812cd0efa1e4e3c488a2f3
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-08 19:57:41 +0300

    Update xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,18 @@
 import XMonad
 
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+
 main :: IO ()
-main = xmonad def
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+
+myConfig = def
+    { modMask  = mod4Mask     -- Rebind Mod to the Super key
+    , terminal = "alacritty"
+    }
+  `additionalKeysP`
+    -- [ ("M-S-z", spawn "xscreensaver-command -lock")
+    [ ("M-C-s", unGrab *> spawn "scrot -s"        )
+    , ("M-f"  , spawn "firefox"                   )
+    ]
