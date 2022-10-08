80da6e64a75baec104660834f658786ed048a937
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-08 20:04:49 +0300

    Add Xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,16 @@
+-- Based on https://xmonad.org/TUTORIAL.html#customizing-xmonad
+
 import XMonad
 
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
 
 myConfig = def
     { modMask  = mod4Mask     -- Rebind Mod to the Super key
