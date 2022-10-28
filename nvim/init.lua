b22936c22daac1df7e4354cb3b3ebdf5b0df36df
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-28 20:21:31 +0300

    Extract Neovim plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 require('nasse.options')
 require('nasse.keymaps')
+require('nasse.plugins')
 
 vim.api.nvim_create_user_command('Q', 'q', {})
 vim.api.nvim_create_user_command('W', 'write', {})
@@ -23,19 +24,3 @@ vim.g.firenvim_config = {
     }
   }
 }
-
--- packer.nvim
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' -- Packer can manage itself
-
-  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end } -- Embed Neovim in Chrome, Firefox, Thunderbird and many other pieces of software.
-  use 'kana/vim-textobj-entire' -- Vim plugin: Text objects for entire buffer
-  use 'kana/vim-textobj-user' -- Vim plugin: Create your own text objects
-  use 'tpope/vim-commentary' -- commentary.vim: comment stuff out
-  use 'tpope/vim-fugitive' -- fugitive.vim: A Git wrapper so awesome, it should be illegal
-  use 'tpope/vim-repeat' -- repeat.vim: enable repeating supported plugin maps with "."
-  use 'tpope/vim-surround' -- surround.vim: quoting/parenthesizing made simple
-  use 'tpope/vim-unimpaired' -- unimpaired.vim: Pairs of handy bracket mappings
-end)
