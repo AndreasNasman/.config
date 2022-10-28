ce5e1ee265db18e3797fd25b1c848c4f688017c6
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-28 21:18:51 +0300

    Format Lua files

--- nvim/lua/nasse/options.lua
+++ nvim/lua/nasse/options.lua
@@ -1,20 +1,20 @@
 local options = {
-  clipboard = "unnamedplus",
-  cursorline = true,
-  expandtab = true,
-  ignorecase = true,
-  number = true,
-  relativenumber = true,
-  shiftwidth = 2,
-  smartcase = true,
-  smartindent = true,
-  splitbelow = true,
-  splitright = true,
-  tabstop = 2,
-  undofile = true,
-  wrap = false,
+	clipboard = "unnamedplus",
+	cursorline = true,
+	expandtab = true,
+	ignorecase = true,
+	number = true,
+	relativenumber = true,
+	shiftwidth = 2,
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	tabstop = 2,
+	undofile = true,
+	wrap = false,
 }
 
 for key, value in pairs(options) do
-  vim.opt[key] = value
+	vim.opt[key] = value
 end
