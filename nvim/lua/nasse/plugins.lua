6b73bae3a50b1eaf182897425c9421c0842c9a7a
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-28 21:41:05 +0300

    Organize plugins

--- nvim/lua/nasse/plugins.lua
+++ nvim/lua/nasse/plugins.lua
@@ -16,14 +16,18 @@ return require("packer").startup({
 			run = function()
 				vim.fn["firenvim#install"](0)
 			end,
-		}) 
-		use("kana/vim-textobj-entire") -- Vim plugin: Text objects for entire buffer
-		use("kana/vim-textobj-user") -- Vim plugin: Create your own text objects
-		use("tpope/vim-commentary") -- commentary.vim: comment stuff out
-		use("tpope/vim-fugitive") -- fugitive.vim: A Git wrapper so awesome, it should be illegal
-		use("tpope/vim-repeat") -- repeat.vim: enable repeating supported plugin maps with "."
-		use("tpope/vim-surround") -- surround.vim: quoting/parenthesizing made simple
-		use("tpope/vim-unimpaired") -- unimpaired.vim: Pairs of handy bracket mappings
+		})
+		use({
+			"kana/vim-textobj-entire", -- Vim plugin: Text objects for entire buffer
+			"kana/vim-textobj-user", -- Vim plugin: Create your own text objects
+		})
+		use({
+			"tpope/vim-commentary", -- commentary.vim: comment stuff out
+			"tpope/vim-fugitive", -- fugitive.vim: A Git wrapper so awesome, it should be illegal
+			"tpope/vim-repeat", -- repeat.vim: enable repeating supported plugin maps with "."
+			"tpope/vim-surround", -- surround.vim: quoting/parenthesizing made simple
+			"tpope/vim-unimpaired", -- unimpaired.vim: Pairs of handy bracket mappings
+		})
 	end,
 
 	-- https://github.com/wbthomason/packer.nvim#using-a-floating-window
