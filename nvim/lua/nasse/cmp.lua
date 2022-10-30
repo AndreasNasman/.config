39236a21b709f983044ebcee295f1725c100b12e
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-10-30 15:34:04 +0200

    Add SuperTab for Neovim completion

--- nvim/lua/nasse/cmp.lua
+++ nvim/lua/nasse/cmp.lua
@@ -4,6 +4,14 @@ require("luasnip/loaders/from_vscode").lazy_load()
 -- https://github.com/hrsh7th/nvim-cmp
 local cmp = require("cmp")
 
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -20,6 +28,26 @@ cmp.setup({
 		["<C-e>"] = cmp.mapping.abort(),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "luasnip" },
