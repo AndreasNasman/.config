5782fca13d8bd4ef872052e45318aef1a15afb54
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-11-13 21:21:26 +0200

    Remove tab completion selection in snippets

--- nvim/lua/nasse/cmp.lua
+++ nvim/lua/nasse/cmp.lua
@@ -29,19 +29,19 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
+			elseif cmp.visible() then
+				cmp.select_prev_item()
 			else
 				fallback()
 			end
 		end, { "i", "s" }),
 		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+			if luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif cmp.visible() then
+				cmp.select_next_item()
 			elseif has_words_before() then
 				cmp.complete()
 			else
