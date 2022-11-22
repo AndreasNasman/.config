6a5273891126f662f9ac05773e9f29f78f5fe240
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-11-22 20:11:31 +0200

    Revert to inline documentation

--- nvim/lua/nasse/autocommands.lua
+++ nvim/lua/nasse/autocommands.lua
@@ -1,3 +1,6 @@
+-- `:help lua-highlight`
+-- https://alpha2phi.medium.com/neovim-for-beginners-lua-autocmd-and-keymap-functions-3bdfe0bebe42#fa3b
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	command = "silent! lua vim.highlight.on_yank()",
 	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
