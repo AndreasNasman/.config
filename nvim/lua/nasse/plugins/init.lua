55b182596e2b5a9039808a121aeb673beab0b660
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-11-25 15:22:56 +0200

    Add Lua LSP completion

--- nvim/lua/nasse/plugins/init.lua
+++ nvim/lua/nasse/plugins/init.lua
@@ -20,6 +20,7 @@ require("packer").startup({
 			"hrsh7th/cmp-buffer", --  nvim-cmp source for buffer words
 			"hrsh7th/cmp-cmdline", --  nvim-cmp source for vim's cmdline
 			"hrsh7th/cmp-nvim-lsp", --  nvim-cmp source for neovim builtin LSP client
+			"hrsh7th/cmp-nvim-lua", --  nvim-cmp source for nvim lua
 			"hrsh7th/cmp-path", --  nvim-cmp source for path
 			"hrsh7th/nvim-cmp", -- A completion plugin for neovim coded in Lua.
 		})
