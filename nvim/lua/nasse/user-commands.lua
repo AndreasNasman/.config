d5548c9d716ad5b21f5990f32f6a450a530c8016
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-01 13:54:31 +0200

    Add reload config functionality

--- nvim/lua/nasse/user-commands.lua
+++ nvim/lua/nasse/user-commands.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_create_user_command("ReloadConfig", function()
+	return require("nasse.custom.reload-config").reloadConfig()
+end, {})
+
 -- Case-insensitive versions of common commands.
 vim.api.nvim_create_user_command("Q", "quit", {})
 vim.api.nvim_create_user_command("QA", "qall", {})
