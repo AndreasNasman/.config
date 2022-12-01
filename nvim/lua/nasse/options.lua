b727b0b2cf044e343541a11e08cb1714d3629adc
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-01 07:21:59 +0200

    Simplify setting options

--- nvim/lua/nasse/options.lua
+++ nvim/lua/nasse/options.lua
@@ -1,30 +1,27 @@
+-- https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/#vim-opt
 vim.cmd([[colorscheme tokyonight-night]])
 
-local options = {
-	clipboard = "unnamedplus",
-	-- https://github.com/hrsh7th/nvim-cmp#recommended-configuration
-	completeopt = "menu,menuone,noselect",
-	cursorline = true,
-	expandtab = true,
-	ignorecase = true,
-	number = true,
-	relativenumber = true,
-	shiftwidth = 2,
-	signcolumn = "yes",
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	tabstop = 2,
-	termguicolors = true,
-	undofile = true,
-	-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#highlight-symbol-under-cursor
-	-- `250` seems like a common low functioning value for `updatetime`.
-	-- Lowering it causes problems when running e.g. `ReloadConfig`.
-	updatetime = 300,
-	wrap = false,
-}
+local set = vim.opt
 
-for key, value in pairs(options) do
-	vim.opt[key] = value
-end
+set.clipboard = "unnamedplus"
+-- https://github.com/hrsh7th/nvim-cmp#recommended-configuration
+set.completeopt = "menu,menuone,noselect"
+set.cursorline = true
+set.expandtab = true
+set.ignorecase = true
+set.number = true
+set.relativenumber = true
+set.shiftwidth = 2
+set.signcolumn = "yes"
+set.smartcase = true
+set.smartindent = true
+set.splitbelow = true
+set.splitright = true
+set.tabstop = 2
+set.termguicolors = true
+set.undofile = true
+-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#highlight-symbol-under-cursor
+-- `250` seems like a common low functioning value for `updatetime`.
+-- Lowering it causes problems when running e.g. `ReloadConfig`.
+set.updatetime = 300
+set.wrap = false
