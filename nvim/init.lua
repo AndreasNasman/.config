986ca4a479db955abfe3c384ad0c20e76a77cfdf
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-01 07:11:07 +0200

    Rename Neovim completion file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 require("nasse.options")
 require("nasse.keymaps")
 require("nasse.plugins")
-require("nasse.cmp")
+require("nasse.completion")
 require("nasse.commands")
 require("nasse.autocommands")
 require("nasse.lsp")
