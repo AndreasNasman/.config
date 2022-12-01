b87b22f88bcb49c88be93129174843d5435fce0c
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-01 08:10:56 +0200

    Remove keymap for opening Neovim options
    Can be replaced with a global mark, e.g. `mC`.

--- nvim/lua/nasse/keymaps.lua
+++ nvim/lua/nasse/keymaps.lua
@@ -17,12 +17,6 @@ set(
 	":Explore $XDG_CONFIG_HOME<CR>",
 	vim.tbl_deep_extend("force", silent, { desc = "Open .config directory" })
 )
-set(
-	"n",
-	"<Leader>os",
-	":edit $XDG_CONFIG_HOME/nvim/lua/nasse/options.lua<CR>",
-	vim.tbl_deep_extend("force", silent, { desc = "Edit Neovim options" })
-)
 set("n", "<Leader><C-w><C-f>", "<C-w>_<C-w>|", { desc = "Fullscreen window" })
 set("n", "<Leader><C-w>f", "<C-w>_<C-w>|", { desc = "Fullscreen window" })
 set("v", "<Leader>d", '"_d', { desc = "Void delete" })
