193f4ecbeb1264a61ea5439f6cd534c892df7ac8
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-01 07:39:28 +0200

    Rename options variable

--- nvim/lua/nasse/options.lua
+++ nvim/lua/nasse/options.lua
@@ -1,27 +1,27 @@
 -- https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/#vim-opt
 vim.cmd([[colorscheme tokyonight-night]])
 
-local set = vim.opt
+local opt = vim.opt
 
-set.clipboard = "unnamedplus"
+opt.clipboard = "unnamedplus"
 -- https://github.com/hrsh7th/nvim-cmp#recommended-configuration
-set.completeopt = "menu,menuone,noselect"
-set.cursorline = true
-set.expandtab = true
-set.ignorecase = true
-set.number = true
-set.relativenumber = true
-set.shiftwidth = 2
-set.signcolumn = "yes"
-set.smartcase = true
-set.smartindent = true
-set.splitbelow = true
-set.splitright = true
-set.tabstop = 2
-set.termguicolors = true
-set.undofile = true
+opt.completeopt = "menu,menuone,noselect"
+opt.cursorline = true
+opt.expandtab = true
+opt.ignorecase = true
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 2
+opt.signcolumn = "yes"
+opt.smartcase = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop = 2
+opt.termguicolors = true
+opt.undofile = true
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#highlight-symbol-under-cursor
 -- `250` seems like a common low functioning value for `updatetime`.
 -- Lowering it causes problems when running e.g. `ReloadConfig`.
-set.updatetime = 300
-set.wrap = false
+opt.updatetime = 300
+opt.wrap = false
