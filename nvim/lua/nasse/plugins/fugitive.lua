d8ac6045647069e77699c531838559f58acd3675
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-02 21:33:35 +0200

    Convert vim-fugitive command to Lua

--- nvim/lua/nasse/plugins/fugitive.lua
+++ nvim/lua/nasse/plugins/fugitive.lua
@@ -1,2 +1,5 @@
 -- https://github.com/tpope/vim-fugitive/issues/2057#issuecomment-1260136745
-vim.cmd([[au FileType gitcommit setlocal spell]])
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "gitcommit" },
+	command = "setlocal spell",
+})
