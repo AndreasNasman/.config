2f00b2232ccc63b93e4f95442d6dee5ae007fee5
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-03 23:46:36 +0200

    Fix formatting

--- nvim/lua/nasse/plugins/telescope.lua
+++ nvim/lua/nasse/plugins/telescope.lua
@@ -2,29 +2,29 @@ local set = vim.keymap.set
 local builtin = require("telescope.builtin")
 set("n", "<Leader>ff", builtin.find_files, { desc = "Search for files (respecting .gitignore)" })
 set(
-  "n",
-  "<Leader>fg",
-  builtin.live_grep,
-  { desc = "Search for a string and get results live as you type, respects .gitignore" }
+	"n",
+	"<Leader>fg",
+	builtin.live_grep,
+	{ desc = "Search for a string and get results live as you type, respects .gitignore" }
 )
 set(
-  "n",
-  "<Leader>fb",
-  builtin.buffers,
-  { desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" }
+	"n",
+	"<Leader>fb",
+	builtin.buffers,
+	{ desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" }
 )
 set(
-  "n",
-  "<Leader>fh",
-  builtin.help_tags,
-  { desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" }
+	"n",
+	"<Leader>fh",
+	builtin.help_tags,
+	{ desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" }
 )
 
 require("telescope").setup({
-  defaults = {
-    -- https://github.com/nvim-telescope/telescope.nvim#layout-display
-    layout_config = {
-      horizontal = { width = 0.95 },
-    },
-  },
+	defaults = {
+		-- https://github.com/nvim-telescope/telescope.nvim#layout-display
+		layout_config = {
+			horizontal = { width = 0.95 },
+		},
+	},
 })
