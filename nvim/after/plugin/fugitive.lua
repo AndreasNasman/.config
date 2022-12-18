5dac2aeaba850e4618eb0d99a175e30c5679f8d6
Andreas Näsman <andreas.z.nasman@gmail.com>
2022-12-18 15:26:45 +0200

    Add vim-fugitive mapping

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,7 @@
+local set = vim.keymap.set
+
+set("n", "<Leader>g", vim.cmd.Git)
+
 -- https://github.com/tpope/vim-fugitive/issues/2057#issuecomment-1260136745
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "gitcommit" },
