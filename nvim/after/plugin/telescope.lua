79783e2062ba6a02bd50aafe3c1ae79b34f87ab4
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-01-02 12:47:12 +0200

    Map Telescope manpage viewer

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -6,6 +6,7 @@ set("n", "<Leader>fb", builtin.buffers, { desc = "Lists open buffers in current 
 set("n", "<Leader>ff", builtin.find_files, { desc = "Search for files (respecting .gitignore)" })
 set("n", "<Leader>fg", builtin.live_grep, { desc = "Search for a string and get results live as you type, respects .gitignore" })
 set("n", "<Leader>fh", builtin.help_tags, { desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" })
+set("n", "<Leader>fm", builtin.man_pages, { desc = "Lists manpage entries, opens them in a help window on `<cr>`" })
 set("n", "<Leader>fo", builtin.oldfiles, { desc = "Lists previously open files, opens on `<cr>`" })
 set("n", "<Leader>fs", builtin.git_status, { desc = "Lists git status for current directory" })
 -- stylua: ignore end
