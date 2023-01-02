ae0631cff963f510224be3c555f933a7d13f3d3d
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-01-02 12:45:45 +0200

    Format Telescope keymap

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,27 +1,14 @@
 local set = vim.keymap.set
 local builtin = require("telescope.builtin")
 
-set(
-	"n",
-	"<Leader>fb",
-	builtin.buffers,
-	{ desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" }
-)
+-- stylua: ignore start
+set("n", "<Leader>fb", builtin.buffers, { desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" })
 set("n", "<Leader>ff", builtin.find_files, { desc = "Search for files (respecting .gitignore)" })
-set(
-	"n",
-	"<Leader>fg",
-	builtin.live_grep,
-	{ desc = "Search for a string and get results live as you type, respects .gitignore" }
-)
-set(
-	"n",
-	"<Leader>fh",
-	builtin.help_tags,
-	{ desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" }
-)
+set("n", "<Leader>fg", builtin.live_grep, { desc = "Search for a string and get results live as you type, respects .gitignore" })
+set("n", "<Leader>fh", builtin.help_tags, { desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" })
 set("n", "<Leader>fo", builtin.oldfiles, { desc = "Lists previously open files, opens on `<cr>`" })
 set("n", "<Leader>fs", builtin.git_status, { desc = "Lists git status for current directory" })
+-- stylua: ignore end
 
 require("telescope").setup({
 	defaults = {
