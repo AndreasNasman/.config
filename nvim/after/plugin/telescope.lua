a6b01bfc8a2f7253fe1a1f8a3d397dd6396dc6b7
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-01-24 13:38:16 +0200

    Update Telescope bindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,12 +4,13 @@ local set = vim.keymap.set
 
 -- stylua: ignore start
 set("n", "<Leader>fb", builtin.buffers, { desc = "Lists open buffers in current neovim instance, opens selected buffer on `<cr>`" })
+set("n", "<Leader>fd", builtin.lsp_document_symbols, { desc = "Lists git status for current directory" })
 set("n", "<Leader>ff", builtin.find_files, { desc = "Search for files (respecting .gitignore)" })
-set("n", "<Leader>fg", builtin.live_grep, { desc = "Search for a string and get results live as you type, respects .gitignore" })
+set("n", "<Leader>fg", builtin.git_status, { desc = "Lists git status for current directory" })
 set("n", "<Leader>fh", builtin.help_tags, { desc = "Lists available help tags and opens a new window with the relevant help info on `<cr>`" })
 set("n", "<Leader>fm", builtin.man_pages, { desc = "Lists manpage entries, opens them in a help window on `<cr>`" })
 set("n", "<Leader>fo", builtin.oldfiles, { desc = "Lists previously open files, opens on `<cr>`" })
-set("n", "<Leader>fs", builtin.git_status, { desc = "Lists git status for current directory" })
+set("n", "<Leader>fs", builtin.live_grep, { desc = "Search for a string and get results live as you type, respects .gitignore" })
 -- stylua: ignore end
 
 -- https://github.com/nvim-telescope/telescope.nvim/issues/2041
