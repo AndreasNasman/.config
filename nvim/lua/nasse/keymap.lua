76ae94b4d74c3b7e0619edce107465571e48d5cd
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-02-03 16:04:45 +0200

    Organize Neovim keymap file

--- nvim/lua/nasse/keymap.lua
+++ nvim/lua/nasse/keymap.lua
@@ -9,13 +9,6 @@ end
 
 -- stylua: ignore start
 
--- Overrides that keep the cursor in the middle of the screen.
-set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half a screen and with the cursor in the middle of the screen" })
-set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half a screen and with the cursor in the middle of the screen" })
-set("n", "G", "Gzz", { desc = "Go to the last line with the cursor in the middle of the screen" })
-set("n", "n", "nzz", { desc = "Repeat the latest search with the cursor in the middle of the screen" })
-set("n", "N", "Nzz", { desc = "Repeat the latest search in the opposite direction with the cursor in the middle of the screen" })
-
 -- Essentials.
 set("n", "<Leader>/", "/\\v", { desc = "Search using very magic" })
 set("n", "<Leader><C-w><C-f>", fullscreen_window, { desc = "Fullscreen window" })
@@ -26,6 +19,13 @@ set("n", "<Leader>oc", function() vim.cmd.Explore("$XDG_CONFIG_HOME") end, vim.t
 set("n", "<Leader>s", vim.cmd.write, { desc = "Save current buffer" })
 -- Doesn't seem possible to write with with `vim.cmd.normal`.
 set("v", ".", ':normal .<CR>', { desc = "Repeat the dot command over visual selection" })
+
+-- Overrides that keep the cursor in the middle of the screen.
+set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half a screen and with the cursor in the middle of the screen" })
+set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half a screen and with the cursor in the middle of the screen" })
+set("n", "G", "Gzz", { desc = "Go to the last line with the cursor in the middle of the screen" })
+set("n", "n", "nzz", { desc = "Repeat the latest search with the cursor in the middle of the screen" })
+set("n", "N", "Nzz", { desc = "Repeat the latest search in the opposite direction with the cursor in the middle of the screen" })
 
 -- Register utilities.
 set("", "<Leader>d", '"_d', { desc = "Delete to black hole register" })
