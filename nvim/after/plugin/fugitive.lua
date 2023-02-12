48e1c34542f0df88515b69c74012730c8c836fef
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-02-12 23:19:31 +0200

    Inline `vim-fugitive` setup

--- nvim/after/plugin/fugitive.lua
+++
@@ -1,9 +0,0 @@
-local set = vim.keymap.set
-
-set("n", "<Leader>g", vim.cmd.Git)
-
--- https://github.com/tpope/vim-fugitive/issues/2057#issuecomment-1260136745
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "gitcommit" },
-	command = "setlocal spell",
-})
