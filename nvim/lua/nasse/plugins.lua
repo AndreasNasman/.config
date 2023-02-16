ac5f8b93f5d237da031b437ff34a628b74ed8fe5
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-02-16 09:41:21 +0200

    Refactor `firenvim` setup

--- nvim/lua/nasse/plugins.lua
+++ nvim/lua/nasse/plugins.lua
@@ -170,11 +170,14 @@ require("lazy").setup({
 	},
 
 	-- Miscellaneous
-	-- {
-	-- 	"glacambre/firenvim", -- Embed Neovim in Chrome, Firefox, Thunderbird and many other pieces of software.
-	-- 	cond = not not vim.g.started_by_firenvim,
-	-- 	build = function()
-	-- 		vim.fn["firenvim#install"](0)
-	-- 	end,
-	-- },
+	{
+		"glacambre/firenvim",
+		config = function()
+			vim.fn["firenvim#install"](0)
+		end,
+
+		-- Lazy load firenvim
+		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+		-- cond = not not vim.g.started_by_firenvim,
+	},
 })
