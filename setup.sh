63956481f9d7077e70e1de21f08c7382a1be3b9f
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-02-19 20:37:49 +0200

    Add script for first time set up

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+# Setup Oh My Zsh plugins through Git submodules.
+git submodule init && git submodule update
+
+cd home
+# Copy files in directories to their correct place in `$HOME`.
+DIRECTORIES_TO_HOME=$(find . -mindepth 1 -type d)
+echo $DIRECTORIES_TO_HOME \
+  | xargs -I {} cp --recursive --no-target-directory {} $HOME/{}
+# Copy files to `$HOME`.
+FILES_TO_HOME=$(find . -maxdepth 1 -type f -printf "%f\n")
+echo $FILES_TO_HOME \
+  | xargs -I {} rsync {} /home/nasse/{}
+# Hardlink files copied above.
+FILES_TO_HARDLINK=$(find . -type f -printf "%P\n")
+echo $FILES_TO_HARDLINK \
+  | xargs -I {} ln -f $HOME/{} {}
