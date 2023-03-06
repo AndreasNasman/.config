e4b3945b9b330e1872c29505bedf47f9031167bb
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-06 16:56:43 +0200

    Fix keybinding

--- nvim/lua/nasse/keymap.lua
+++ nvim/lua/nasse/keymap.lua
@@ -14,7 +14,7 @@ set("n", "<Leader>/", "/\\v", { desc = "Search using very magic" })
 set("n", "<Leader><C-w><C-f>", fullscreen_window, { desc = "Fullscreen window" })
 set("n", "<Leader><C-w>f", fullscreen_window, { desc = "Fullscreen window" })
 set("n", "<Leader>e", vim.cmd.Explore, vim.tbl_deep_extend("force", opts, { desc = "Explore directory of current file" }))
-set("n", "<Leader>l", function() vim.cmd.set("spell!") end, opts, { desc = "Toggle spell checking" })
+set("n", "<Leader>l", function() vim.cmd.set("spell!") end, { desc = "Toggle spell checking" })
 set("n", "<Leader>oc", function() vim.cmd.Explore("$XDG_CONFIG_HOME") end, vim.tbl_deep_extend("force", opts, { desc = "Open .config directory" }))
 set("n", "<Leader>s", vim.cmd.write, { desc = "Save current buffer" })
 -- Doesn't seem possible to write with with `vim.cmd.normal`.
