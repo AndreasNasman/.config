5bdb65e5c07f9136557c41dfe38f7c16b4226140
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-16 21:03:15 +0200

    Convert setup comments to echoes

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env zsh
 
-# Setup Oh My Zsh plugins through Git submodules.
+echo '📦 Setting up Oh My Zsh plugins through Git submodules.'
 git submodule init && git submodule update
 
 cd home
-# Copy files in directories to their correct place in `$HOME`.
+echo '📁 Copying files in directories to their correct place in `$HOME`.'
 DIRECTORIES_TO_HOME=$(find . -mindepth 1 -type d)
 echo $DIRECTORIES_TO_HOME \
   | xargs -I {} cp --recursive --no-target-directory {} $HOME/{}
-# Copy files to `$HOME`.
+echo '💾 Copying files to `$HOME`.'
 FILES_TO_HOME=$(find . -maxdepth 1 -type f -printf "%f\n")
 echo $FILES_TO_HOME \
   | xargs -I {} rsync {} /home/nasse/{}
-# Hardlink files copied above.
+echo '🔗 Hardlink copied files.'
 FILES_TO_HARDLINK=$(find . -type f -printf "%P\n")
 echo $FILES_TO_HARDLINK \
   | xargs -I {} ln -f $HOME/{} {}
 
-# Create XDG directories.
+echo '📂 Creating XDG directories.'
 mkdir -p $XDG_CACHE_HOME
 mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_STATE_HOME
