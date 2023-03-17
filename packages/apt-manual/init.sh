834b114a1279431e8792dca246fea4e5e1943c90
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-17 13:05:32 +0200

    Update package scripts

--- packages/apt-manual/init.sh
+++ packages/apt-manual/init.sh
@@ -2,5 +2,4 @@
 
 echo '\n🎣 Installing manually fetched packages.'
 CURRENT_FILE_NAME=$(basename $0)
-# https://unix.stackexchange.com/a/109222
 find . -mindepth 1 -executable -not -name $CURRENT_FILE_NAME -exec '{}' \;
--- packages/apt-manual/neovim.sh
+++ packages/apt-manual/neovim.sh
@@ -2,16 +2,16 @@
 
 # https://lindevs.com/install-fd-command-for-finding-files-on-ubuntu
 echo '\n🎁 Installing `fd`.'
-FD_VERSION=$(curl -s "https://api.github.com/repos/sharkdp/fd/releases/latest" | grep --only-matching --perl-regexp '"tag_name": "v\K[0-9.]+')
+FD_VERSION=$(curl --silent "https://api.github.com/repos/sharkdp/fd/releases/latest" | grep --only-matching --perl-regexp '"tag_name": "v\K[0-9.]+')
 FD_EXECUTABLE="fd.deb"
-curl -Lo $FD_EXECUTABLE "https://github.com/sharkdp/fd/releases/latest/download/fd_${FD_VERSION}_amd64.deb"
+curl --location --output $FD_EXECUTABLE "https://github.com/sharkdp/fd/releases/latest/download/fd_${FD_VERSION}_amd64.deb"
 sudo apt install --yes ./$FD_EXECUTABLE
-rm -rf $FD_EXECUTABLE
+rm --recursive --force $FD_EXECUTABLE
 
 # https://lindevs.com/install-ripgrep-on-ubuntu
 echo '\n🎁 Installing `ripgrep`.'
-RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep --only-matching --perl-regexp '"tag_name": "\K[0-9.]+')
+RIPGREP_VERSION=$(curl --silent "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep --only-matching --perl-regexp '"tag_name": "\K[0-9.]+')
 RIPGREP_EXECUTABLE="ripgrep.deb"
-curl -Lo $RIPGREP_EXECUTABLE "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep_${RIPGREP_VERSION}_amd64.deb"
+curl --location --output $RIPGREP_EXECUTABLE "https://github.com/BurntSushi/ripgrep/releases/latest/download/ripgrep_${RIPGREP_VERSION}_amd64.deb"
 sudo apt install --yes ./$RIPGREP_EXECUTABLE
-rm -rf $RIPGREP_EXECUTABLE
+rm --recursive --force $RIPGREP_EXECUTABLE
