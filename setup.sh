301668fd9af8215a96b1052f9eaf231d9608b4a9
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-17 15:57:19 +0200

    Improve `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,32 +1,25 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 echo '📦 Installing Oh My Zsh plugins through Git submodules.'
 git submodule init && git submodule update
 
 echo '📂 Creating XDG directories.'
+source home/.zshenv
 mkdir -p $XDG_CACHE_HOME
 mkdir -p $XDG_DATA_HOME
 mkdir -p $XDG_STATE_HOME
 
 cd $XDG_CONFIG_HOME/home
-echo '📁 Copying files in directories to their correct place in `$HOME`.'
-DIRECTORIES_TO_HOME=$(find . -mindepth 1 -type d)
-echo $DIRECTORIES_TO_HOME \
-  | xargs -I '{}' cp --recursive --no-target-directory '{}' $HOME/'{}'
 echo '💾 Copying files to `$HOME`.'
-FILES_TO_HOME=$(find . -maxdepth 1 -type f -printf "%f\n")
-echo $FILES_TO_HOME \
-  | xargs -I '{}' rsync '{}' /home/nasse/'{}'
+find . -type f -exec rsync '{}' /home/nasse/'{}' \;
 echo '🔗 Hardlinking copied files.'
-FILES_TO_HARDLINK=$(find . -type f -printf "%P\n")
-echo $FILES_TO_HARDLINK \
-  | xargs -I '{}' ln -f $HOME/'{}' '{}'
+find . -type f -exec ln -f $HOME/'{}' '{}' \;
 
 echo '📰 Updating and upgrading installed packages.'
 sudo apt update --yes
 sudo apt upgrade --yes
 
-echo '\n📦 Setting up packages.'
+echo -e '\n📦 Setting up packages.'
 cd $XDG_CONFIG_HOME/packages/apt
 source init.sh
 cd $XDG_CONFIG_HOME/packages/apt-manual
@@ -34,18 +27,27 @@ source init.sh
 cd $XDG_CONFIG_HOME/packages/make
 source init.sh
 
-echo '\n🐚 Setting Zsh as the default shell.'
-chsh -s $(which zsh)
+echo -e '\n🔭 Installing `fzf`.'
+cd $XDG_CONFIG_HOME
+git clone --quiet --depth 1 https://github.com/junegunn/fzf.git
+./fzf/install --xdg --no-key-bindings --no-completion --no-update-rc
 
-echo '\n💻 Installing Oh My Zsh with existing config.'
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mv $XDG_CONFIG_HOME/zsh/.zshrc.pre-oh-my-zsh $XDG_CONFIG_HOME/zsh/.zshrc
+echo -e '\n💻 Installing Oh My Zsh with existing config.'
+cd $HOME
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
-echo '\n🗑️ Removing preinstalled files.'
+echo -e '\n📜 Installing Node v16.'
+/bin/zsh -i -c 'nvm install 16'
+
+echo -e '\n🪄 Opening Neovim for setup.'
+/bin/zsh -i -c 'nvim'
+
+echo -e '\n🗑️ Removing preinstalled files.'
 rm --force $HOME/.bash_logout
 rm --force $HOME/.bashrc
 rm --force $HOME/.motd_shown
 rm --force $HOME/.profile
 
-echo '\n🪄 Opening Neovim for setup.'
-nvim
+echo -e '\n🐚 Setting Zsh as the default shell.'
+chsh -s $(which zsh)
+exec zsh
