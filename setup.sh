e6fd747a6e57c85f0b10ceff76f439e1f1be9147
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-17 13:26:29 +0200

    Add installed package update and upgrade setup step

--- setup.sh
+++ setup.sh
@@ -22,7 +22,11 @@ FILES_TO_HARDLINK=$(find . -type f -printf "%P\n")
 echo $FILES_TO_HARDLINK \
   | xargs -I '{}' ln -f $HOME/'{}' '{}'
 
-echo '📦 Setting up packages.'
+echo '📰 Updating and upgrading installed packages.'
+sudo apt update --yes
+sudo apt upgrade --yes
+
+echo '\n📦 Setting up packages.'
 cd $XDG_CONFIG_HOME/packages/apt
 source init.sh
 cd $XDG_CONFIG_HOME/packages/apt-manual
