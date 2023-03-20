812bb7890c44a94426019b276e162f439758e8b6
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-03-20 12:37:10 +0200

    Improve setup scripts

--- setup/main.sh
+++ setup/main.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
+SETUP_DIRECTORY=$(dirname $(realpath $0))
+cd $SETUP_DIRECTORY
+
 echo -e '\n📦 Setting up packages.'
 cd $SETUP_DIRECTORY/apt
 source init.sh
@@ -9,6 +12,8 @@ cd $SETUP_DIRECTORY/make
 source init.sh
 
 echo -e '\n📜 Installing Node v16.'
+REPO_PATH=$(git rev-parse --show-toplevel)
+source $REPO_PATH/zsh/.zshrc
 nvm install 16
 
 echo -e '\n🪄 Opening Neovim for setup.'
