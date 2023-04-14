e47c0c835912a9b3bc274aa1115419d0b6bedfdd
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-14 16:10:28 +0300

    Apply WSL clipboard settings for Neovim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- `:help clipboard-wsl`
+vim.g.clipboard = {
+  cache_enabled = 0,
+  copy = {
+    ["*"] = "clip.exe",
+    ["+"] = "clip.exe",
+  },
+  name = "WslClipboard",
+  paste = {
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+}
