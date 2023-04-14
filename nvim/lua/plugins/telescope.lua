99dd6e46d99558b09cd3c12172944c425a26027f
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-14 17:40:22 +0300

    Improve Telescope layout

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,8 +3,17 @@ return {
   dependencies = {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
-    config = function()
+    config = function(_, opts)
       require("telescope").load_extension("fzf")
+      require("telescope").setup(opts)
     end,
+    opts = {
+      defaults = {
+        layout_config = {
+          vertical = { height = 0.99, width = 0.99 },
+        },
+        layout_strategy = "vertical",
+      },
+    },
   },
 }
