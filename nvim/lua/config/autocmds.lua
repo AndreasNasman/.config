d1e56f6ef86e819f583ec0e67fe02c3b087eada6
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-14 16:56:56 +0300

    Make cursor blink in Neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- This autocommand persist cursor blinking in the terminal when quitting Neovim.
+-- Configuring `guicursor` in `options.lua` is insufficient.
+vim.api.nvim_create_autocmd("VimLeave", {
+  command = "set guicursor+=a:blinkon100",
+})
