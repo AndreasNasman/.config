a4c4838ece04af317346e315b3c460a7b0db7242
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-16 20:31:16 +0300

    Organize options file

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,20 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- `:help clipboard-wsl`
-vim.g.clipboard = {
-  cache_enabled = 0,
-  copy = {
-    ["*"] = "clip.exe",
-    ["+"] = "clip.exe",
-  },
-  name = "WslClipboard",
-  paste = {
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-}
-
 -- https://www.reddit.com/r/neovim/comments/10ra30e/comment/j6vi70r/
 local function reset_to_default(accessor, option)
   accessor[option] = vim.api.nvim_get_option_info2(option, {}).default
@@ -43,6 +29,20 @@ api.nvim_create_user_command("Wa", "wall", {})
 
 api.nvim_create_user_command("WQ", "wq", {})
 api.nvim_create_user_command("Wq", "wq", {})
+
+-- `:help clipboard-wsl`
+vim.g.clipboard = {
+  cache_enabled = 0,
+  copy = {
+    ["*"] = "clip.exe",
+    ["+"] = "clip.exe",
+  },
+  name = "WslClipboard",
+  paste = {
+    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+}
 
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#borders
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
