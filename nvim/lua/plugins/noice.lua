318a202c4846cbadefa55bf89b1be326e1bdffb7
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-16 22:50:51 +0300

    Make LSP progress indicator transparent

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -4,5 +4,13 @@ return {
     presets = {
       lsp_doc_border = true,
     },
+    -- https://github.com/folke/noice.nvim/issues/226#issuecomment-1340758028
+    views = {
+      mini = {
+        win_options = {
+          winblend = 0,
+        },
+      },
+    },
   },
 }
