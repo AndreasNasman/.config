9758d7d4ac8852b0c4dbcc3d6fffc60367022602
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-16 22:50:38 +0300

    Add keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local set = vim.keymap.set
+
+-- stylua: ignore start
+set("", "<leader>d", '"_d', { desc = "Delete to the black hole register" })
+set("", "<leader>y", '"+y', { desc = "Copy to the system clipboard" })
+set("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = "Copy the unnamed register value to the system clipboard" })
+set("n", "<leader>fp", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = "Copy file path to the system clipboard" })
+set("v", ".", ":normal .<cr>", { desc = "Repeat last change in visual mode" })
+set("v", "<leader>p", '"_c<C-r>"<ESC>', { desc = "Paste to the black hole register" })
