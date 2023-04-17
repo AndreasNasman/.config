8a58ebcd1decbbcc827dc9f2aa8dcf12e3abc21f
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-17 15:35:50 +0300

    Tweak CursorLine color to math Deathstar wallpaper

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,6 +1,12 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
+    on_highlights = function(highlight)
+      local purple_space = "#351a35"
+      highlight.CursorLine = {
+        bg = purple_space,
+      }
+    end,
     styles = {
       floats = "transparent",
       sidebars = "transparent",
