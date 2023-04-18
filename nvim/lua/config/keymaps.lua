55372409df7fce93b36cc385b1042c8cb595bc15
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-18 23:25:57 +0300

    Add keymap for navigating hunks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,8 @@ local set = vim.keymap.set
 -- stylua: ignore start
 set("", "<leader>d", '"_d', { desc = "Delete to the black hole register" })
 set("", "<leader>y", '"+y', { desc = "Copy to the system clipboard" })
+set("n", "<C-j>", "<cmd>Gitsigns next_hunk<cr>", { desc = "Jump to next hunk." })
+set("n", "<C-k>", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk." })
 set("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = "Copy the unnamed register value to the system clipboard" })
 set("n", "<leader>fpa", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Copy absolute file path to the system clipboard" })
 set("n", "<leader>fpr", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = "Copy relative file path to the system clipboard" })
