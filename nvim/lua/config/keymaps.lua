c97ce8071e29c3c0ddc09b7cb0571c6903733e7b
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-20 13:40:24 +0300

    Improve keymaps
    - Add `"+Y` keymap.
    - Simplify keymap descriptions.
    - Sort keymaps case-insensitively.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,12 +5,13 @@
 local set = vim.keymap.set
 
 -- stylua: ignore start
-set("", "<leader>d", '"_d', { desc = "Delete to the black hole register" })
-set("", "<leader>y", '"+y', { desc = "Copy to the system clipboard" })
-set("n", "<C-j>", "<cmd>Gitsigns next_hunk<cr>", { desc = "Jump to next hunk." })
-set("n", "<C-k>", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk." })
-set("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = "Copy the unnamed register value to the system clipboard" })
-set("n", "<leader>fpa", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = "Copy absolute file path to the system clipboard" })
-set("n", "<leader>fpr", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = "Copy relative file path to the system clipboard" })
-set("v", ".", ":normal .<cr>", { desc = "Repeat last change in visual mode" })
-set("v", "<leader>p", '"_c<C-r>"<ESC>', { desc = "Paste to the black hole register" })
+set("", "<leader>d", '"_d', { desc = 'Delete ("_)' })
+set("", "<leader>y", '"+y', { desc = 'Copy (y) ("+)' })
+set("", "<leader>Y", '"+Y', { desc = 'Copy (Y) ("+)' })
+set("n", "<C-j>", "<cmd>Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
+set("n", "<C-k>", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
+set("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = '"" => "+' })
+set("n", "<leader>fpa", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = 'Copy file path (absolute) ("+)' })
+set("n", "<leader>fpr", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = 'Copy file path (relative) ("+)' })
+set("v", ".", ":normal .<cr>", { desc = "Repeat last change" })
+set("v", "<leader>p", '"_c<C-r>"<ESC>', { desc = 'Paste ("_)' })
