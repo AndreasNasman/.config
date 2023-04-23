d289c46c341da6de0e55ab2c76b527ca7aa3bafa
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-23 23:57:03 +0300

    Add completion cycling
    - Cycle through completion sources by `group_index`.
    - Cycles can start from automatically opened completion windows.
    - Cycles are resumed from manually closed completion windows.
    - Cycling through empty completion windows (e.g. Copilot loading) is
      possible.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,3 +1,43 @@
+---@param sources cmp.SourceConfig[]
+local function create_lookups_from_sources(sources)
+  local group_index_to_sources = {}
+  local group_indexes = {}
+
+  for _, source in ipairs(sources) do
+    local group_index = source.group_index
+    local existing_sources = group_index_to_sources[group_index]
+
+    if existing_sources then
+      table.insert(existing_sources, source)
+    else
+      if not group_index then
+        error("All sources must have a defined `group_index`.")
+      end
+      group_index_to_sources[group_index] = { source }
+      table.insert(group_indexes, group_index)
+    end
+  end
+
+  table.sort(group_indexes)
+
+  local index_to_sources = {}
+  local source_name_to_index = {}
+
+  for index, group_index in ipairs(group_indexes) do
+    local sources_by_group_index = group_index_to_sources[group_index]
+    table.insert(index_to_sources, sources_by_group_index)
+
+    for _, source in ipairs(sources_by_group_index) do
+      source_name_to_index[source.name] = index
+    end
+  end
+
+  return {
+    source_name_to_index = source_name_to_index,
+    index_to_sources = index_to_sources,
+  }
+end
+
 return {
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
@@ -41,7 +81,61 @@ return {
       end, { "i", "s" }),
     })
 
+    local lookups = create_lookups_from_sources(opts.sources)
+    local source_name_to_index = lookups.source_name_to_index
+    local index_to_sources = lookups.index_to_sources
+    local first_index = 1
+    local last_index = vim.tbl_count(index_to_sources)
+    local index = first_index
+    local should_open_current_source = true
+
+    ---Adjust index when completion window is opened automatically.
+    ---@param arg { window: cmp.CustomEntriesView }
+    cmp.event:on("menu_opened", function(arg)
+      local first_entry = arg.window.get_first_entry(arg.window)
+      if first_entry.context.option.reason == cmp.ContextReason.Auto then
+        index = source_name_to_index[first_entry.source.name]
+      end
+    end)
+
+    ---Preserve the current source when the completion window is closed
+    ---manually.
+    ---@param arg { window: cmp.CustomEntriesView }
+    cmp.event:on("menu_closed", function(arg)
+      local first_entry = arg.window.get_first_entry(arg.window)
+      if first_entry then
+        should_open_current_source = true
+      end
+    end)
+
+    ---Preserve the current source when a completion is accepted.
+    cmp.event:on("confirm_done", function()
+      should_open_current_source = true
+    end)
+
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-Space>"] = cmp.mapping(function()
+        if should_open_current_source then
+          -- No index adjustment is needed when the current source should be
+          -- (re)opened.
+          should_open_current_source = false
+        else
+          -- Find the index of the next source to open.
+          -- Adjusting the index before opening the next source allows cycling
+          -- through empty sources, e.g. Copilot that is still loading.
+          if index == last_index then
+            index = first_index
+          else
+            index = index + 1
+          end
+        end
+
+        cmp.complete({
+          config = {
+            sources = index_to_sources[index],
+          },
+        })
+      end, { "i", "s" }),
       ["<CR>"] = cmp.mapping.confirm({ select = false }),
       ["<C-l>"] = cmp.mapping.confirm({
         behavior = cmp.ConfirmBehavior.Replace,
