9e06ff9ec7001c03cc992e52ee2cfc375b759f87
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-23 23:33:34 +0300

    Utilize LazyVim map keymap function

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local set = vim.keymap.set
 local unmap = vim.cmd.unmap
 
 unmap("<C-h>")
@@ -11,14 +10,28 @@ unmap("<S-h>")
 unmap("<S-l>")
 unmap("gw")
 
+-- https://www.lazyvim.org/configuration/general#keymaps
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
 -- stylua: ignore start
-set("", "<leader>d", '"_d', { desc = 'Delete ("_)' })
-set("", "<leader>y", '"+y', { desc = 'Copy (y) ("+)' })
-set("", "<leader>Y", '"+y$', { desc = 'Copy (Y) ("+)' })
-set("n", "<C-j>", "<cmd>Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
-set("n", "<C-k>", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
-set("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = '"" => "+' })
-set("n", "<leader>pa", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = 'Copy file path (absolute) ("+)' })
-set("n", "<leader>pr", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = 'Copy file path (relative) ("+)' })
-set("v", ".", ":normal .<cr>", { desc = "Repeat last change" })
-set("v", "<leader>p", '"_c<C-r>"<ESC>', { desc = 'Paste ("_)' })
+map("", "<leader>d", '"_d', { desc = 'Delete ("_)' })
+map("", "<leader>y", '"+y', { desc = 'Copy (y) ("+)' })
+map("", "<leader>Y", '"+y$', { desc = 'Copy (Y) ("+)' })
+map("n", "<C-j>", "<cmd>Gitsigns next_hunk<cr>", { desc = "Jump to next hunk" })
+map("n", "<C-k>", "<cmd>Gitsigns prev_hunk<cr>", { desc = "Jump to previous hunk" })
+map("n", "<leader>+", function() vim.fn.setreg("+", vim.fn.getreg('"')) end, { desc = '"" => "+' })
+map("n", "<leader>pa", function() vim.fn.setreg("+", vim.fn.expand("%:p")) end, { desc = 'Copy file path (absolute) ("+)' })
+map("n", "<leader>pr", function() vim.fn.setreg("+", vim.fn.expand("%:~:.")) end, { desc = 'Copy file path (relative) ("+)' })
+map("v", ".", ":normal .<cr>", { desc = "Repeat last change" })
+map("v", "<leader>p", '"_c<C-r>"<ESC>', { desc = 'Paste ("_)' })
