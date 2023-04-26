4346a48eeeb104b8e6bc4e67a245358db2b44351
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-04-26 12:38:10 +0300

    Trip branch name when in cambri2 repo

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,6 +6,20 @@ return {
         { "mode" },
         { "searchcount" },
       },
+      lualine_b = {
+        {
+          "branch",
+          -- https://github.com/nvim-lualine/lualine.nvim#component-options
+          fmt = function(str)
+            if os.getenv("PWD") == os.getenv("CAMBRI_TOOL") then
+              -- Only display `CT-1234` branch name part when in the `cambri2` repo.
+              return str:sub(6, 12)
+            else
+              return str
+            end
+          end,
+        },
+      },
     },
   },
 }
