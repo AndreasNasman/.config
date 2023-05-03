6108eb903cea9e043d9581c2ee982bb76d52abfc
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-05-03 14:38:08 +0300

    Add vim-fugitive for Git file blame
    Looks like this feature won't be added to gitsigns.
    https://github.com/lewis6991/gitsigns.nvim/issues/508#issuecomment-1112390198

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
+    { import = "plugins/custom" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
