355418c25667246abb53cc76788c2c4862e17695
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-09-01 12:27:58 +0300

    Add Neovim edit from fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,7 @@ starship init fish | source
 
 # https://docs.brew.sh/Manpage#shellenv-bashcshfishpwshshtcshzsh
 eval "$(brew shellenv)"
+
+# https://fishshell.com/docs/current/interactive.html#shared-bindings
+set EDITOR nvim
+set VISUAL nvim
