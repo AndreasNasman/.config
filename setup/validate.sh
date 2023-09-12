b2dc1919a2176c133943ad3dd1158b27451b6d48
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-09-12 16:17:44 +0300

    Improve setup scripts

--- setup/validate.sh
+++ setup/validate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env fish
 
 # https://trello.com/c/QfYFXbJ7
-cp /usr/local/opt/yabai/share/yabai/examples/yabairc ../yabai/
-cp /usr/local/opt/yabai/share/yabai/examples/skhdrc ../skhd/
+cp /usr/local/opt/yabai/share/yabai/examples/yabairc (path resolve (status dirname)/../yabai/)
+cp /usr/local/opt/yabai/share/yabai/examples/skhdrc (path resolve (status dirname)/../skhd/)
