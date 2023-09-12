1ccb5b259072bcec4181afa6cb5e960d7fcd98cf
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-09-12 15:37:44 +0300

    Update validate setup script

--- setup/validate.sh
+++ setup/validate.sh
@@ -2,3 +2,7 @@
 
 # https://starship.rs/presets/pastel-powerline.html#configuration
 starship preset pastel-powerline -o ~/.config/starship.toml
+
+# https://trello.com/c/QfYFXbJ7
+cp /usr/local/opt/yabai/share/yabai/examples/yabairc ../yabai/
+cp /usr/local/opt/yabai/share/yabai/examples/skhdrc ../skhd/
