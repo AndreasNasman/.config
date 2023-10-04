7a0932547868d8bdde7586521a18f7a358917d3d
Andreas Näsman <andreas.z.nasman@gmail.com>
2023-10-04 15:25:45 +0300

    Implicitly check JS files with tsserver

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,4 +5,16 @@ return {
     -- `:help lspconfig-highlight`
     require("lspconfig.ui.windows").default_options.border = "rounded"
   end,
+  opts = {
+    servers = {
+      tsserver = {
+        settings = {
+          implicitProjectConfiguration = {
+            -- https://www.reddit.com/r/neovim/comments/132ax85/comment/jiacvuy/
+            checkJs = true,
+          },
+        },
+      },
+    },
+  },
 }
