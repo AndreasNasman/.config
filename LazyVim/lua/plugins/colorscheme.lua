802d729b60a8bf557d1b2f53919d883be68f72d8
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2023-12-14 00:04:38 +0200

    Change to transparent Catppuccin theme
    fixup
    
    fixup

---
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    -- https://www.lazyvim.org/configuration#icons--colorscheme
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+    },
+  },
+}
---
+++ LazyVim/lua/plugins/notify.lua
@@ -0,0 +1,7 @@
+return {
+  "rcarriga/nvim-notify",
+  opts = {
+    -- Suppress `nvim-notify` warning when saving with a transparent color scheme.
+    background_colour = "#000000",
+  },
+}
