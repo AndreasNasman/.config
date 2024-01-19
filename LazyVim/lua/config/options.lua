f4a8d656234f529a6b264555ae07512377bd988c
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-01-20 00:11:53 +0200

    Reset `clipboard` option to default

--- LazyVim/lua/config/options.lua
+++ LazyVim/lua/config/options.lua
@@ -4,6 +4,13 @@
 
 local opt = vim.opt
 
+-- https://www.reddit.com/r/neovim/comments/10ra30e/comment/j6vi70r/
+local function reset_to_default(option)
+  opt[option] = vim.api.nvim_get_option_info2(option, {}).default
+end
+
+reset_to_default("clipboard")
+
 -- Re-enable the cursor blinking after running a command.
 -- `1000` milliseconds is the system default that kitty is configured to use.
 opt.guicursor:append("a:blinkon1000")
