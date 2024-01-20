90a3aa8d47ee54f13eb9df8e124fb372bdff128f
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-01-21 00:15:14 +0200

    Configure Telescope

---
+++ LazyVim/lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  "telescope.nvim",
+  opts = {
+    defaults = {
+      layout_config = {
+        vertical = {
+          height = 0.99,
+          width = 0.99,
+        },
+      },
+      layout_strategy = "vertical",
+    },
+  },
+}
