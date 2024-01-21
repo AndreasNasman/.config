f28a5f6c90dae5b8326c088f09779d9ec2751d33
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-01-21 15:40:22 +0200

    Show hidden files in Neo-tree

--- LazyVim/lua/plugins/neo-tree.lua
+++ LazyVim/lua/plugins/neo-tree.lua
@@ -1,6 +1,11 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
     window = {
       position = "right",
     },
