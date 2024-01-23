3745beb33b1415c69c05c3028ff1836c9aa10e85
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-01-23 18:51:43 +0200

    Disable unwanted plugins

---
+++ LazyVim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", opts = { enable = false } },
+}
