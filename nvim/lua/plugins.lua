a9825b95ec57120e4bc7be151aba3ff7d2659ba3
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-04-12 10:56:58 +0300

    Add catppuccin theme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -5,4 +5,13 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({})
+require('lazy').setup({
+    {
+        'catppuccin/nvim',
+        init = function()
+            vim.cmd.colorscheme('catppuccin-mocha')
+        end,
+        name = 'catppuccin',
+        priority = 1000,
+    },
+})
