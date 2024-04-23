f6b5c9c29bbfd250cb8c105a93b9b4679b2f67c5
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-04-23 17:58:02 +0300

    Add Telescope file browser

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -174,18 +174,25 @@ require('lazy').setup({
         branch = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-file-browser.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
             'nvim-telescope/telescope-ui-select.nvim',
             'nvim-tree/nvim-web-devicons',
         },
         config = function()
-            require('telescope').setup({
-                defaults = { layout_config = { height = 0.99, width = 0.99 } },
-                extensions = { ['ui-select'] = { require('telescope.themes').get_dropdown() } },
+            local telescope = require('telescope')
+            telescope.setup({
+                defaults = {
+                    layout_config = { height = 0.99, width = 0.99 },
+                },
+                extensions = {
+                    ['ui-select'] = { require('telescope.themes').get_dropdown() },
+                },
             })
 
-            require('telescope').load_extension('fzf')
-            require('telescope').load_extension('ui-select')
+            telescope.load_extension('file_browser')
+            telescope.load_extension('fzf')
+            telescope.load_extension('ui-select')
         end,
     },
     { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} },
