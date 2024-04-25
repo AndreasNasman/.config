cc572c7f5010383067ea3b1fa45449b4196ea47b
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-04-26 01:22:50 +0300

    Add friendly snippets

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -27,7 +27,18 @@ return {
             'saadparwaiz1/cmp_luasnip',
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
-            { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
+            {
+                'L3MON4D3/LuaSnip',
+                build = 'make install_jsregexp',
+                dependencies = {
+                    {
+                        'rafamadriz/friendly-snippets',
+                        config = function()
+                            require('luasnip.loaders.from_vscode').lazy_load()
+                        end,
+                    },
+                },
+            },
         },
         config = function()
             local cmp = require('cmp')
