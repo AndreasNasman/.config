977cb6253f64ba62ec400fec43c8d78d8b002686
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-04-26 00:44:35 +0300

    Refactor plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,12 @@ require('options')
 require('keymaps')
 require('autocommands')
 require('user-commands')
-require('plugins')
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('plugins')
