c48708fe046d3c57d6ed73f4eab838f85e46a840
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-04-26 01:20:29 +0300

    Move install colorscheme config to correct place

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,4 +10,4 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', { install = { colorscheme = { 'catppuccin' } } })
