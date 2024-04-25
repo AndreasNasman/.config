8352a1dc6cc362aa73db59380fb0de0f3976779b
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-04-26 00:44:35 +0300

    Refactor plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,4 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup({
+return {
     {
         'catppuccin/nvim',
         init = function()
@@ -433,4 +426,4 @@ require('lazy').setup({
     },
 }, {
     install = { colorscheme = { 'catppuccin' } },
-})
+}
