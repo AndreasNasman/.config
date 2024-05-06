b9aed437f4a6f9c21fd92714ae222241513478eb
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-05-06 18:56:34 +0300

    Update lazy.nvim init with documentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,15 @@ require('autocommands')
 require('user-commands')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        'https://github.com/folke/lazy.nvim.git',
+        lazypath,
+    })
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
