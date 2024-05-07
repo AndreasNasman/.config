cdc6a2b31dd652dea88864066632e38b249c07a2
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-05-07 22:08:47 +0300

    Reorder plugin properties

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -10,24 +10,6 @@ return {
     },
     {
         'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-path',
-            {
-                'L3MON4D3/LuaSnip',
-                build = 'make install_jsregexp',
-                dependencies = {
-                    {
-                        'rafamadriz/friendly-snippets',
-                        config = function()
-                            require('luasnip.loaders.from_vscode').lazy_load()
-                        end,
-                    },
-                },
-            },
-        },
         config = function()
             local luasnip = require('luasnip')
             luasnip.config.setup({})
@@ -71,6 +53,24 @@ return {
                 },
             })
         end,
+        dependencies = {
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            {
+                'L3MON4D3/LuaSnip',
+                build = 'make install_jsregexp',
+                dependencies = {
+                    {
+                        'rafamadriz/friendly-snippets',
+                        config = function()
+                            require('luasnip.loaders.from_vscode').lazy_load()
+                        end,
+                    },
+                },
+            },
+        },
+        event = 'InsertEnter',
     },
     {
         'stevearc/conform.nvim',
@@ -98,17 +98,16 @@ return {
     { 'lewis6991/gitsigns.nvim', opts = {} },
     {
         'neovim/nvim-lspconfig',
-        dependencies = {
-            { 'j-hui/fidget.nvim', opts = {} },
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-            'WhoIsSethDaniel/mason-tool-installer.nvim',
-            { 'folke/neodev.nvim', opts = {} },
-        },
         config = function()
             vim.api.nvim_create_autocmd('LspAttach', {
                 group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
                 callback = function(event)
+                    local map = function(keys, func, desc)
+                        vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+                    end
+                    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+                    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
                     if client and client.server_capabilities.documentHighlightProvider then
                         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -153,6 +152,13 @@ return {
                 },
             })
         end,
+        dependencies = {
+            { 'j-hui/fidget.nvim', opts = {} },
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            'WhoIsSethDaniel/mason-tool-installer.nvim',
+            { 'folke/neodev.nvim', opts = {} },
+        },
     },
     {
         'echasnovski/mini.nvim',
