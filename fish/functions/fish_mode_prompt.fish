140d504cb138082cf90a96156db61984f077c5bc
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-05-08 15:19:19 +0300

    Create own fish prompt
    No need for Oh My Post anymore.

--- fish/functions/fish_mode_prompt.fish
+++ fish/functions/fish_mode_prompt.fish
@@ -1,3 +1,21 @@
 function fish_mode_prompt
- 
+    switch $fish_bind_mode
+        case default replace_one
+            set -l catppuccin_blue 89b4fa
+            set_color --bold black --background $catppuccin_blue
+            echo ' '
+        case insert
+            set -l catppuccin_green A6E3A1
+            set_color --bold black --background $catppuccin_green
+            echo ' '
+        case visual
+            set -l catppuccin_mauve CBA6F7
+            set_color --bold black --background $catppuccin_mauve
+            echo ' '
+        case '*'
+            set -l catppuccin_red F38BA8
+            set_color --bold $catppuccin_red
+            echo '?'
+    end
+    set_color normal
 end
