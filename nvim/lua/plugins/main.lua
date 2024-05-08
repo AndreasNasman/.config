e871423cf3bc20bbe9b41fb170dbe92cc1ceb20e
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-05-08 15:39:59 +0300

    Load Neogit when command is run

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -46,6 +46,7 @@ return {
     },
     {
         'NeogitOrg/neogit',
+        cmd = 'Neogit',
         dependencies = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' },
         opts = { kind = 'split' },
     },
