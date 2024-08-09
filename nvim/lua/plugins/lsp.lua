8401cda2a18f6b76a4f4618900e3d71d31746331
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-08-09 09:58:12 +0300

    Update cursor highlight from kickstarter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -151,15 +151,27 @@ return {
                     --stylua: ignore end
 
                     local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
+                    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+                        local highlight_augroup = vim.api.nvim_create_augroup('nasse-lsp-highlight', { clear = false })
+
                         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
                             buffer = event.buf,
                             callback = vim.lsp.buf.document_highlight,
+                            group = highlight_augroup,
                         })
 
                         vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
                             buffer = event.buf,
                             callback = vim.lsp.buf.clear_references,
+                            group = highlight_augroup,
+                        })
+
+                        vim.api.nvim_create_autocmd('LspDetach', {
+                            callback = function(event2)
+                                vim.lsp.buf.clear_references()
+                                vim.api.nvim_clear_autocmds({ group = 'nasse-lsp-highlight', buffer = event2.buf })
+                            end,
+                            group = vim.api.nvim_create_augroup('nasse-lsp-detach', { clear = true }),
                         })
                     end
                 end,
