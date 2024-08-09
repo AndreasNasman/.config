07148f360d5ce8eecd4fec1cfe0a7b3b0384e354
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-08-09 10:17:45 +0300

    Update lazy.nvim bootstrapping with error handling from kickstarter.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,14 +5,11 @@ require('user-commands')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        '--branch=stable',
-        'https://github.com/folke/lazy.nvim.git',
-        lazypath,
-    })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        error('Error cloning lazy.nvim:\n' .. out)
+    end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
