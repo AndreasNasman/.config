aaab82a4d88ad87a78d91e964be2e7517f1c41f0
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-10-11 10:22:37 +0300

    Remove LazyVim and kickstart.nvim

--- LazyVim/lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-context", opts = { enable = false } },
-}
