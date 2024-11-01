299aa3ff8007f2d30bad2df2098befaf1950ae89
Andreas Näsman <andreas.z.nasman@gmail.com>
2024-11-01 09:11:51 +0200

    Shorten fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-    printf ' %s%s%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd --dir-length=0) (set_color $fish_color_operator) (fish_git_prompt '  %s') (set_color normal)
+    printf ' %s%s%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd --dir-length=1 --full-length-dirs=2) (set_color $fish_color_operator) (fish_git_prompt '  %s') (set_color normal)
 end
