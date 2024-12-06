dd698df63367353d57ff18d1aa30ffc67c1d9227
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2024-12-07 00:16:46 +0200

    Remove remnant from multiple Neovim distros

--- fish/config.fish
+++ fish/config.fish
@@ -47,7 +47,7 @@ alias rm='rm -i'
 
 # Use `command` to avoid an infinite recursion loop.
 # https://fishshell.com/docs/2.0/index.html#syntax-function-wrappers
-alias nvim="set --erase NVIM_APPNAME; command nvim --listen /tmp/nvim-server.$KITTY_WINDOW_ID"
+alias nvim="command nvim --listen /tmp/nvim-server.$KITTY_WINDOW_ID"
 
 # Use the `nvim` alias to benefit from its function.
 alias v.='nvim .'
