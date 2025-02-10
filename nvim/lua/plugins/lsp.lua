99fde39e2ee46a4ccd005c361504ed0dda8724c9
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-02-10 09:55:50 +0200

    Update lazydev dependency
    https://github.com/nvim-lua/kickstart.nvim/commit/5bdde24dfb353d365d908c5dd700f412ed2ffb17

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,11 +1,10 @@
 local utils = require('utils')
 
 return {
-    { 'Bilal2453/luvit-meta', lazy = true },
     {
         'folke/lazydev.nvim',
         ft = 'lua',
-        opts = { library = { { path = 'luvit-meta/library', words = { 'vim%.uv' } } } },
+        opts = { library = { { path = '${3rd}/luv/library', words = { 'vim%.uv' } } } },
     },
     {
         'mfussenegger/nvim-lint',
