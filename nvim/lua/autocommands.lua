48643edb4b9dd7bfc35a6b41ceff24c81a3aa808
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-03-02 11:29:52 +0200

    Display relative file paths for Oil

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('nasse-highlight-yank', { clear = true }),
 })
+
+-- https://github.com/stevearc/oil.nvim/issues/234
+vim.api.nvim_create_autocmd('BufWinLeave', {
+    callback = function()
+        vim.cmd.cd('.')
+    end,
+    group = vim.api.nvim_create_augroup('nasse-relative-path-fix', { clear = true }),
+    pattern = { 'oil:///*', 'NvimTree*' },
+})
