58253f7eab90c0865aeca349a3f49640c7f470ee
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-04-21 23:20:55 +0300

    Add Android Studio with Homebrew through Nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -63,6 +63,7 @@
           homebrew = {
             casks = [
               "1password"
+              "android-studio"
               "grammarly-desktop"
               "notion"
               "parallels"
