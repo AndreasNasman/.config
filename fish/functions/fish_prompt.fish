0cba220680b8c97d471fb31f7e03f87d43a3b78b
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-06-12 17:07:37 +0300

    Show more Git status in fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,5 @@
 function fish_prompt
+    set -g __fish_git_prompt_show_informative_status true
+    set -g __fish_git_prompt_showcolorhints true
     printf ' %s%s%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd --dir-length=1 --full-length-dirs=2) (set_color $fish_color_operator) (fish_git_prompt '  %s') (set_color normal)
 end
