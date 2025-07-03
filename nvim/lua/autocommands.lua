8e5a49c0c9bfda87141100ba9932d7095dcc7d89
Andreas Näsman <AndreasNasman@users.noreply.github.com>
2025-07-03 09:40:17 +0300

    Update vim.highlight to vim.hl

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank()
+        vim.hl.on_yank()
     end,
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('nasse-highlight-yank', { clear = true }),
